41fa059effb7d3aa5f88a1611314a995b60097b6
Josh <seapolco@gmail.com>
2022-12-10 17:41:41 +0000

    Day 5: Part 1 complete!

--- day05.mjs
+++ day05.mjs
@@ -31,7 +31,7 @@ crates.forEach((e) => {
             if(crateObj[index] === undefined) {
                 crateObj[index] = [char]
             } else {
-                crateObj[+index].push(char)
+                crateObj[+index].unshift(char)
             } 
         }
      }
@@ -39,5 +39,48 @@ crates.forEach((e) => {
     }
 })
 
+let indexs= [1,2,3,4,5,6,7,8,9]
+
+for(let key in crateObj) {
+  let index = indexs.shift()
+  console.log(index)
+  crateObj[index] = crateObj[key]
+  if(index !== 1) {
+    delete crateObj[key]
+  }
+}
+
 //crateObj['1'].pop()
-console.log(crateObj['1'])
+//console.log(crateObj)
+
+console.log(instructions)
+
+const instrucNums = (instruction) => {
+    let nums = instruction.split(' ').filter((e) => Number(e)).join(',')
+    return nums.split(',').map((e) => +e)
+  }
+
+instructions.shift();
+// console.log(instructions[0])
+
+// instructions.forEach((e) => {
+//     console.log(instrucNums(e))
+// })
+
+let simples = instructions.map((e) => instrucNums(e))
+
+//console.log(simples)
+
+function elfOrders(arr, obj){
+    let moveCount = arr[0];
+    
+    while(moveCount > 0) {
+      obj[arr[2]].push(obj[arr[1]].pop())
+      moveCount--
+    }
+}
+
+simples.forEach((e) => elfOrders(e, crateObj));
+
+console.log(crateObj)
+
